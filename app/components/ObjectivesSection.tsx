314e096bfd683e23a5a24a8d3d45aeef53dec940
david-rosval <20b3010135@untels.edu.pe>
2025-02-18 19:22:21 -0500

    dropdown to the objectives

--- app/components/ObjectivesSection.tsx
+++ app/components/ObjectivesSection.tsx
@@ -1,37 +1,80 @@
 import { Link, useLoaderData } from "@remix-run/react"
 import { loader } from "../routes/_index"
 import Objective from "./Objective"
-import { CirclePlus } from "lucide-react"
+import { ChevronDown, CirclePlus } from "lucide-react"
 import { ObjectiveDialogForm } from "./DialogForm"
 import { useState } from "react"
-import { AnimatePresence } from "motion/react"
-
+import { motion, AnimatePresence } from "motion/react"
 export default function ObjectivesSection() {
   const weekPlannerData = useLoaderData<typeof loader>()
   const { objectives } = weekPlannerData
 
+  const [dropDown, setDropDown] = useState(false)
+
   const [modal, setModal] = useState(false)
+
+  function toggleDropDown() {
+    setDropDown((dropDown) => !dropDown)
+  }
  
   return (
     <div className="p-5 shadow-lg rounded-lg bg-gray-50 dark:bg-gray-800 flex flex-col gap-5">
-      <div className="flex items-center gap-3">
-        <Link to="/objectives">
-          <h2 className="text-2xl font-semibold hover:underline hover:underline-offset-4">Objectives</h2>
-        </Link>
+      <div className="flex items-center justify-between">
+        <div className="flex gap-3 items-center">
+          <Link to="/objectives">
+            <h2 className="text-2xl font-semibold hover:underline hover:underline-offset-4">Objectives</h2>
+          </Link>
+          <button
+            onClick={() => setModal(true)}
+            >
+            <CirclePlus size={25} className="stroke-gray-500 transition-colors ease-out hover:stroke-green-500" />
+          </button>
+        </div>
+
         <button
-          onClick={() => setModal(true)}
+          onClick={toggleDropDown}
         >
-          <CirclePlus size={25} className="stroke-gray-500 transition-colors ease-out hover:stroke-green-500" />
+          <motion.div
+            animate={{
+              rotate: dropDown ? 180 : 0,
+              transition: {
+                duration: 0.3
+              }
+            }}
+          >
+            <ChevronDown className="stroke-gray-400" size={30} />
+          </motion.div>
         </button>
         
       </div>
-      <div className="responsive-grid-display">
-        {objectives.map((objective, i) => (
-          <Link key={i} to={`/objectives/${objective.id}`}>
-            <Objective objective={objective} />
-          </Link>
-        ))}
-      </div>
+      <AnimatePresence>
+        {dropDown && (
+          <motion.div
+            initial={{
+              height: 0,
+            }}
+            animate={{
+              height: "auto",
+              transition: {
+                duration: 0.2
+              }
+            }}
+            exit={{
+              height: 0,
+              transition: {
+                duration: 0.2
+              }
+            }}
+            className="responsive-grid-display overflow-hidden"
+          >
+            {objectives.map((objective, i) => (
+              <Link key={i} to={`/objectives/${objective.id}`}>
+                <Objective objective={objective} />
+              </Link>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
       {/* Modal */}
       <AnimatePresence>
         {modal && <ObjectiveDialogForm modal={modal} setModal={setModal} />}
